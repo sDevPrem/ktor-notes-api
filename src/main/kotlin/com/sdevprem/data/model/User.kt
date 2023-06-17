4aa7716f71e386e817da8ffa904e1cf027ddb54c
Prem Thakur <sdevpremthakur@gmail.com>
2023-06-17 11:23:36 +0530

    Store hashed password

--- src/main/kotlin/com/sdevprem/data/model/User.kt
+++ src/main/kotlin/com/sdevprem/data/model/User.kt
@@ -5,6 +5,18 @@ import org.ktorm.entity.Entity
 interface User : Entity<User> {
     val id: Int
     val userName: String
-    val password: String
     val email: String
+    val password: String
+
+    fun copy(
+        id: Int = this.id,
+        userName: String = this.userName,
+        email: String = this.email,
+        password: String = this.password
+    ) = copy().apply {
+        if (this.id < 1) this["id"] = id //because primary key start from 1 and can't be changed if set
+        this["userName"] = userName
+        this["email"] = email
+        this["password"] = password
+    }
 }
--- src/main/kotlin/com/sdevprem/data/service/UserService.kt
+++ src/main/kotlin/com/sdevprem/data/service/UserService.kt
@@ -2,6 +2,7 @@ package com.sdevprem.data.service
 
 import com.sdevprem.data.model.User
 import com.sdevprem.data.repository.UserRepository
+import org.mindrot.jbcrypt.BCrypt
 
 class UserService(
     private val userRepository: UserRepository
@@ -12,6 +13,12 @@ class UserService(
     fun getUserByEmail(email: String) =
         userRepository.getUserByEmail(email)
 
-    fun insertUser(user: User) =
-        userRepository.insertUser(user)
+    fun insertUser(user: User): User {
+        val hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt())
+        val newId = userRepository.insertUser(user.copy(password = hashedPassword))
+        return user.copy(id = newId)
+    }
+
+    fun isUserPasswordValid(userPassword: String, dbPassword: String) =
+        BCrypt.checkpw(userPassword, dbPassword)
 }
